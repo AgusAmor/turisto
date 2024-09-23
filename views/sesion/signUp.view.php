89495d339c3e28bfda3f567315804b7f730f33d3
Agustin Amor <agustinwamor@gmail.com>
2024-09-23 17:31:59 -0300

    estilos de form de registro

--- views/sesion/signUp.view.php
+++ views/sesion/signUp.view.php
@@ -4,37 +4,46 @@
         <!-- <img src="img/banner-index.png" alt=""> -->
         <a href="/web-app-turisto/">&#10094</a>
         <fieldset>
-            <div class="campoForm">
-                <label for="name">Nombre <strong>*</strong></label>
-                <input type="text" name="name" id="name" maxlength="20" required />
+            <div class="campoForm" id="nombreApellido">
+                <div>
+                    <label for="name">Nombre <strong>*</strong></label>
+                    <input type="text" name="name" id="name" maxlength="20" required />
+                </div>
 
-                <label for="surname">Apellido <strong>*</strong></label>
-                <input type="text" name="surname" id="surname" maxlength="20" required />
+                <div>
+                    <label for="surname">Apellido <strong>*</strong></label>
+                    <input type="text" name="surname" id="surname" maxlength="20" required />
+                </div>
             </div>
             <div class="campoForm">
                 <label for="nationality">Nacionalidad <strong>*</strong></label>
                 <input type="text" name="nationality" id="nationality" maxlength="30" required />
             </div>
-            <div class="campoForm">
-                <label for="phone">Telefono <strong>*</strong></label>
-                <input type="number" name="phone" id="phone" maxlength="20" required />
+            <div class="campoForm" id="mediosContacto">
+                <div>
+                    <label for="email">Email <strong>*</strong></label>
+                    <input type="email" name="email" id="email" maxlength="50" required />
+                </div>
+                
+                <div>
+                    <label for="phone">Telefono <strong>*</strong></label>
+                    <input type="number" name="phone" id="phone" maxlength="20" required />
+                </div>
             </div>
-            <hr>
-            <div class="campoForm">
-                <label for="email">Email <strong>*</strong></label>
-                <input type="email" name="email" id="email" maxlength="50" required />
+            <div class="campoForm" id="contrasenias">
+               <div>
+                 <label for="pass">Contraseña <strong>*</strong></label>
+                 <input type="password" name="pass" id="pass" maxlength="15" required />
+               </div>
+
+                <div>
+                    <label for="pass2">Confirme su contraseña</label>
+                    <input type="password" name="pass2" id="pass2" maxlength="15" required />
+                </div>
             </div>
-            <div class="campoForm">
-                <label for="pass">Contraseña <strong>*</strong></label>
-                <input type="password" name="pass" id="pass" maxlength="15" required />
-            </div>
-            <div class="campoForm">
-                <label for="pass2">Confirme su contraseña</label>
-                <input type="password" name="pass2" id="pass2" maxlength="15" required />
-            </div>
-            <div class="campoForm">
+            <div id="terminosContenedor">
                 <input type="checkbox" name="terms" id="terms" required />
-                <label for="terms">He leído y estoy de acuerdo con los <a href="" id="terms">Términos y condiciones</a></label>
+                <label for="terms">He leído y estoy de acuerdo con los <a href="" id="terminos">Términos y condiciones</a></label>
             </div>
             <div class="campoForm">
                 <input type="submit" value="Registrarse" id="registrarse">
